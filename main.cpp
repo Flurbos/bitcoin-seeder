13821ff4a6be86ba69e8e153c9ca2b0211d6539f
TheCreator <31379149+Flurbos@users.noreply.github.com>
2017-10-22 16:41:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -342,7 +342,7 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"flurblocks.mooo.com", "dnsseed.flurblocks.mooo.com"};
+static const string mainnet_seeds[] = {"dnsseed.flurbo.xyz", ""};
 static const string testnet_seeds[] = {""};
 static const string *seeds = mainnet_seeds;
 
